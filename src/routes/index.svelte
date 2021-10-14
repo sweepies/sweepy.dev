a397141b86d93a18fe3507d47a6f4f1967e9f39e
Sweepyoface <sweepy@sweepy.dev>
2021-10-14 00:46:29 -0700

    updates - new, simpler design - moved to tailwind css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,76 +1,61 @@
 <script>
-    import Fa from 'svelte-fa/src/fa.svelte'
-    import { faGithub, faTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons"
-    import { faArrowDown, faEnvelope } from "@fortawesome/free-solid-svg-icons"
+    import Fa from "svelte-fa/src/fa.svelte";
+    import {
+        faGithub,
+        faTwitter,
+        faDiscord,
+    } from "@fortawesome/free-brands-svg-icons";
+    import { faClipboard } from "@fortawesome/free-solid-svg-icons";
+    import Clipboard from "svelte-clipboard";
+
+    let copied
 </script>
 
-<div id="head" class="container">
-    <div id="title">I'm Sweepy</div>
-    <div id="arrow"><Fa
-        icon={faArrowDown}
-        style="margin-top: 3rem;"
-    /></div>
-    
+<div class="flex flex-col gap-y-16 items-center justify-center">
+    <div class="text-center mt-32 text-7xl md:text-8xl lg:text-9xl">I'm Sweepy</div>
+
+    <div class="flex flex-row gap-x-6 text-6xl md:text-7xl lg:text-8xl">
+        <a href="https://github.com/sweepyoface"><Fa icon={faGithub} /></a>
+        <a href="https://twitter.com/sweepyoface"><Fa icon={faTwitter} /></a>
+
+        <Clipboard
+            text="Sweepy#5624"
+            let:copy
+            on:copy={() => {
+                if (copied) return
+
+                copied = true
+
+                setTimeout(() => {
+                    copied = false
+                }, 1000)
+            }}
+        >
+        <span class="cursor-pointer" on:click={copy}>
+            {#if !copied}
+                <Fa icon={faDiscord} />
+            {:else}
+                <Fa icon={faClipboard} />
+            {/if}
+        </span>
+        </Clipboard>
+    </div>
 </div>
 
-<div class="center">
-    <a href="https://github.com/sweepyoface"><Fa icon={faGithub} style="margin-right: 0.5rem;"/>github</a>
-    <a href="https://twitter.com/sweepyoface"><Fa icon={faTwitter} style="margin-right: 0.5rem;"/>twitter</a>
-    <div><Fa icon={faDiscord} style="margin-right: 0.5rem;"/>Sweepy#5624</div>
+<div class="absolute bottom-0 left-0 w-full overflow-hidden">
+    <svg
+        class="relative block h-64"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+    >
+        <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            style="fill: #313131;"
+        />
+    </svg>
 </div>
-
-<img src="/wave.svg" alt="wave-shape">
 
 <style lang="scss">
-    :global {
-        body {
-            background-color: lightpink;
-            // background-color: lightpink;
-            font-family: "Gaegu", sans-serif;
-            color: #313131;
-        }
-
-        a {
-            color: #313131;
-            text-decoration: none;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-    }
-
-    .container {
-        display: flex;
-        justify-content: space-between;
-        flex-direction: column;
-    }
-    #title {
-        display: flex;
-        font-size: 7rem;
-    }
-    #arrow {
-        display: flex;
-        font-size: 3rem;
-    }
-
-    #head {
-        margin-top: 2rem;
-        align-items: center;
-    }
-
-    .center {
-        font-size: 2rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        margin-top: 5rem;
-    }
-
-    img {
-        pointer-events: none;
-        position: fixed;
-        bottom: 0%;
-    }
 </style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+  mode: "jit",
+  purge: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {},
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
